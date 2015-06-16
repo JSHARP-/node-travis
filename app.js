e7ac79f88d3ecdb2f0f1fc6a84e4340ad5d202a3
Justin Sharp <justin.d.sharp@gmail.com>
2015-06-16 16:28:04 -0600

    added naught

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+
+if (process.send) process.send('online');
+
+process.on('message', function(message) {
+	  if (message === 'shutdown') {
+	  	  //Do whatever you need to do before shutdown (cleanup, saving stuff, etc.)
+	  	  process.exit(0);
+	  }
+});
+
 var express = require('express'),
-	app = express();
+		app = express();
 
 app.get('/', function (req, res) {
-	res.send('<h1>Hello World!</h1>');
+		res.send('<h1>Hello World!</h1>');
 });
 
 var server = app.listen(3000, function () {
-	var host = server.address().address,
+		var host = server.address().address,
 		port = server.address().port;
-	console.log('Example app listening at http://%s:%s', 'localhost', port);
+		console.log('Example app listening at http://%s:%s', 'localhost', port);
 });
