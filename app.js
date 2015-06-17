fd4c53e1eca685b5a89f2ace078e7fcebdde5617
Justin Sharp <justin.d.sharp@gmail.com>
2015-06-17 10:31:33 -0600

    added a submodule

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ process.on('message', function(message) {
 });
 
 var express = require('express'),
+		message = require('./submodule-test/message'),
 		app = express();
 
 app.get('/', function (req, res) {
-		res.send('<h1 style="color:blue;">Helloooooo World!</h1>');
+		res.send('<h1 style="color:blue;">' + message + '</h1>');
 });
 
 var server = app.listen(3000, function () {
